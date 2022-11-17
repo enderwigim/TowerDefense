f26f331acd549ed05a4e0fac3e6b9733ba9bbc94
enderwigim <enderwigim@gmail.com>
2022-11-17 16:56:29 +0100

    Create Game Class and Add Player_Test Class

---
+++ config.py
@@ -0,0 +1,13 @@
+# Screen Related
+WIN_WIDTH, WIN_HEIGHT = 1024, 763
+TILESIZE = 32
+FPS = 60
+
+# Player Related
+PLAYER_LAYER = 1
+PLAYER_SPEED = 3
+# Colors
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
---
+++ game.py
@@ -0,0 +1,67 @@
+import pygame
+from config import *
+from sprites import *
+import sys
+
+class Game:
+    def __init__(self):
+        self.player = None
+        self.all_attacks = None
+        self.all_enemies = None
+        self.all_blocks = None
+        self.playing = None
+        self.all_sprites = None
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        # self.font = pygame.font.Font('Arial', 32)
+        self.running = True
+
+    def new(self):
+        # A new game starts
+        self.playing = True
+        
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.all_blocks = pygame.sprite.LayeredUpdates()
+        self.all_enemies = pygame.sprite.LayeredUpdates()
+        self.all_attacks = pygame.sprite.LayeredUpdates()
+
+        self.all_sprites.add(PlayerTest(self, 1, 2))
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+    def update(self):
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(WHITE)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def main(self):
+        # game loop
+        while self.playing:
+            self.events()
+            self.draw()
+            self.update()
+        self.running = False
+    def game_over(self):
+        pass
+
+    def intro_screen(self):
+        pass
+
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
+
+pygame.quit()
+sys.exit()
