8847f5c10528c96998638dbec042fc1de7b0851e
Santiago Lorenzano <enderwigim@gmail.com>
2022-11-21 18:09:54 +0100

    Add Sprites

---
+++ Assets/Map/Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tiles" tilewidth="32" tileheight="32" tilecount="300" columns="15">
+ <image source="Tiles.png" trans="ff00ff" width="480" height="640"/>
+</tileset>
